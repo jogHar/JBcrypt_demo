d29b1952d028eabf60792f2671cea8e7e26b0e24
Hardik Jogani <joganihardik57@gmail.com>
2019-02-26 18:04:14 +0530

    init commit

---
+++ src/PasswordGenerator.java
@@ -0,0 +1,20 @@
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordGenerator {
+	public static void main(String args[]){
+		String password = "123";
+		String hashedPassword = passwordHash(password);
+		System.out.println(hashedPassword);
+		checkPass(password, hashedPassword);
+		
+	}
+	public static String passwordHash(String password){
+		return BCrypt.hashpw(password, BCrypt.gensalt());
+	}
+	public static void checkPass(String plainPassword, String hashedPassword) {
+		if (BCrypt.checkpw(plainPassword, hashedPassword))
+			System.out.println("Match");
+		else
+			System.out.println("Not Match");
+	}
+}
